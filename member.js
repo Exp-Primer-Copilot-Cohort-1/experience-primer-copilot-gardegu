518622428df7b76f327cafc630f7e6d1a3c1ab15
gardegu <122101100+gardegu@users.noreply.github.com>
2023-10-29 12:59:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+
+    skillsMember.style.display = "block";
+    skills.style.display = "none";
+    member.style.display = "none";
+}
